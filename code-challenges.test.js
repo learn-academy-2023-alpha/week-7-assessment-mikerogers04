69640423d1e7846e44810c0316df62fe76400f2f
mikerogers04 <mikerogers04@hotmail.com>
2023-04-02 17:46:33 -0700

    Completed Assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,6 +10,13 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+//Re-usable Test Template
+// describe("greeter", () => {
+//   it("returns a generic greeting", () => {
+//     expect(greeter()).toEqual("Hello, LEARN student!")
+//   })
+// })
+
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
 
 // a) Create a test with an expect statement using the variable provided.
@@ -21,7 +28,101 @@ const people = [
 ]
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
+describe("fullSentence", () => {
+  it("returns an array with a sentence about each person with their name capitalized", () => {
+    expect(fullSentence(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+
+//Test failed:  ReferenceError: fullSentence is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+
 // b) Create the function that makes the test pass.
+
+//Initial Pseudo:
+//Create a function called fullSentence.
+//Input: An array of objects with key-value pairs for a name and occupation.
+//Need to pull key-value pairs out of objects but keep data in arrays: possible method to do this
+//Need to combine arrays (name + occupation) together
+//Need to capitalize the first letter in the first and last name
+//Output: Returns an array with a sentence about each person with their name capitalized.
+
+//Final iteration of pseudocode inside function. 
+//I was super determined to use object deconstruction to make this function work. It was very difficult and took alot of time and reading documentation. I know you can simply use object dot notation to pull the key-pair values out but, wanted to explore this approach first. The fact that the output had to be one array with the string sentences at each element is what made this approach much more difficult. I knew there was a much more efficient way to do this problem, that refactored function is further below. 
+
+const fullSentence = (array) => {
+    //Empty array for object names
+    let newArray = []
+    //Empty array for object occupations
+    let newArray2 = []
+    //object deconstruction
+    const [element1, element2, element3] = array
+    //pushing each object.name value into an array as arrays using .split
+    newArray.push(element1.name.split(" "), element2.name.split(" "), element3.name.split(" "))
+    //pushing each object.occupation value into an array as arrays using .split
+    newArray2.push(element1.occupation.split(" "), element2.occupation.split(" "), element3.occupation.split(" "))
+    //using .flat to remove nested arrays in name array
+    let nameArray = newArray.flat() 
+    //using .flat to remove nested arrays in occupation array
+    let occupationArray = newArray2.flat()
+    //using .map to iterate over name array to uppercase the first letter of each word
+    let finalArray = nameArray.map(value => 
+          value[0].toUpperCase() + value.substring(1)) 
+    //array deconstruction
+    let [a, b, c, d, e, f] = finalArray
+    //pushed first two elements into occupation array at specified position
+    occupationArray.splice(0, 0, a, b)
+    //pushed next two elements into occupation array at specified position
+    occupationArray.splice(4, 0, c, d)
+    //pushed next two elements into occupation array at specified position
+    occupationArray.splice(10, 0, e, f)
+    //String interpolation for elements 0-3 in occupation array
+    let output1 = (`${occupationArray[0]} ${occupationArray[1]} is ${occupationArray[2]} ${occupationArray[3]}.`)
+    //String interpolation for elements 4-9 in occupation array
+    let output2 = (`${occupationArray[4]} ${occupationArray[5]} is ${occupationArray[6]} ${occupationArray[7]} ${occupationArray[8]} ${occupationArray[9]}.`)
+    //String interpolation for elements 10-14 in occupation array
+    let output3 = (`${occupationArray[10]} ${occupationArray[11]} is ${occupationArray[12]} ${occupationArray[13]} ${occupationArray[14]}.`)
+    //Empty array to store outputs
+    let outputArray = []
+    //Push output 1 into outputArray
+    outputArray.push(output1)
+    //Push output 2 into outputArray
+    outputArray.push(output2)
+    //Push output 3 into outputArray
+    outputArray.push(output3)
+    //Test log to verify if correct content got pushed into array
+    // console.log(outputArray)
+    return outputArray
+}
+
+console.log(fullSentence(people))
+
+//Test output: Passed
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+//Refactored function
+
+//Pseudo:
+//Create a function called fullSentence2.
+//Input: An array of objects with key-pair values of name and occupation
+//Use .map to iterate over array of objects
+  //Pull names out of objects using dot notation and .split to make them arrays
+  //Use .map to iterate over name arrays and uppercase first letter of each element using .toUpperCase and .substring to concatenate strings back together. 
+  //Use .join to combine arrays together and string interpolation both the names and occupations into sentences in one array
+//Output: Returns an array with a sentence about each person with their name capitalized
+
+// const fullSentence2 = (array) => {
+//   return array.map(value => {
+//     let nameValues = value.name.split(" ")
+//     let upperCaseNames = nameValues.map(value => value[0].toUpperCase() + value.substring(1))
+//     // console.log(upperCaseNames)
+//     let output = upperCaseNames.join(" ")
+//     return `${output} is ${value.occupation}.`
+//   })
+// }
+// console.log(fullSentence2(people))
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
@@ -32,9 +133,49 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+describe("remainderArray", () => {
+  it("returns a an array of only the remainders of the number when divided by 3", () => {
+    expect(remainderArray(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(remainderArray(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
+//Test failed:  ReferenceError: remainderArray is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 passed, 2 total
+
+
 // b) Create the function that makes the test pass.
 
+//Pseudo:
+//Create a function called remainderArray.
+//Input: A mixed data array.
+//Iterate over array using .map
+//Check if each element is a number by using typeof
+//If element is a number, then use modulo by 3 to get the remainder
+//Use .filter to remove undefined values out of newArray 
+//Output: Returns an array of only the remainders of the number when divided by 3.
+
+const remainderArray = (array) => {
+  let newArray = []
+  newArray = array.map(value => {
+    if(typeof value === "number"){
+       return value % 3
+    }
+  })
+  return newArray.filter(value => 
+    value !== undefined)
+}
+
+console.log(remainderArray(hodgepodge1))
+console.log(remainderArray(hodgepodge2))
+
+//Test output: Passed
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
+
 
 // a) Create a test with an expect statement using the variables provided.
 
@@ -43,4 +184,36 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe("cubed", () => {
+  it("returns the sum of all the numbers cubed", () => {
+    expect(cubed(cubeAndSum1)).toEqual(99)
+    expect(cubed(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+//Test failed:  ReferenceError: cubed is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 2 passed, 3 total
+
 // b) Create the function that makes the test pass.
+
+//Pseudo:
+//Create a function called cubed.
+//Input: An array of numbers
+//Use .reduce method to accumulate the sum when each element number is cubed.
+//Output: The sum of all the numbers cubed.
+
+const cubed = (array) => {
+  //initial value is an optional argument, but needed for the reducer function in this case to know where to start in the array. 
+  //For some reason the output of cubedSum is 93 for the first text example without the initialValue set
+  let initialValue = 0
+  let cubedValues = array.reduce((cubedSum, currentValue) => cubedSum = cubedSum + (currentValue*currentValue*currentValue), initialValue)
+  return cubedValues
+}
+
+console.log(cubed(cubeAndSum1))
+console.log(cubed(cubeAndSum2))
+
+//Test output: Passed
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,93 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: To fix the mistake, you can create a migration to add in the foriegn key column. 
 
-Researched answer:
+First in the Terminal, you want to run the command: rails migration add_column_cohort_id_to_students
+A migration file will be created. In the migration file you will want to add the following code: add_column :students :cohort_id :integer
+Once the file is saved, run the commad: rails db:migrate 
+This will update the schema file with the newly added column. Verify by checking schema file to see if column was created successfully. 
+
+Researched answer: While this alternative is not best practice, if you have not migrated your database yet. You can go into the schema file itself and add/edit any information for your model. Highly advised not to do this because in my opinion it defeats the purpose of migrations. Also, migrations are a log of all the changes you have made to your model. So, if you go through a lot of migrations and end up coming into problems. You can reference those migration files to check for any issues. 
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: The RESTful routes that always need a param passed are show, edit, update, and delete. Show, update and delete all need an id to do an action on. While edit may return a form to the user, an id is still needed for the database to return which entry to be edited. 
 
-Researched answer:
+Researched answer: Index, new, and create are the only RESTful routes that do not need a param passed. Index does not require params because it is a display route only. Create does not need any params because it is only creating an instance in the database. Show is used for displaying specific instances in the database. New does not need any params because it displays an html form for the user to add information for creation. 
 
 3. Name three rails generator commands. What is created by each?
 
 Your answer:
+1. rails g model Student name:string cohort:string
+    -Creates a model(table) for the Rails application and defining the column names and data types. 
+2. rails g resource Student name:string cohort:string
+    -Creates the model and initial migration, a controller to define CRUD methods, and a routes file for RESTful routes. 
+3. rails g controller Main
+    -Creates a controller file for all CRUD methods to be defined in. 
 
 Researched answer:
+4. rails g rspec:install
+    -Creates the rspec fils needed to run Rspec testing.
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
 action: "GET" location: /students
+Controller Method: /students#index
+This action will show ALL instances in the the database. 
 
 action: "POST" location: /students
+Controller Method: /students#create
+This action will create a single instance in the database.
 
 action: "GET" location: /students/new
+Controller Method: /studnets#new
+This action will show the user a form they can interact with to add a new instance to the database. 
 
 action: "GET" location: /students/2
+Controller Method: /students#show[:id]
+This action will show a specific instance in the database by the provided id number.
 
 action: "GET" location: /students/2/edit
+Controller Method: /students#edit[:id]
+This action will return a form for editing an entry in the database. 
 
 action: "PATCH" location: /students/2
+Controller Method: /students#update[:id]
+This action will update the instance that is provided by the id number. 
 
 action: "DELETE" location: /students/2
+Controller Method: /students#delete[:id]
+This action will delete the instance that is provided by the id number. 
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+Note: Used Blog Post App as a guide  
+
+To-Do List Application
+As a developer, I have tasked myself with creating an application to manage my to do list.
+
+User Story 1: As a user of the application, I need to be able to manage tasks. 
+-Create a resource for tasks with the given information: task name, comepleted or not completed tasks, and date
+User Story 2: As a user, I can view all entries in the database
+User Story 3: As a user, I can create new tasks in the database
+User Story 4: As a user, I can an edit/update existing tasks if they are completed or not
+User Story 5: As a user, I can delete any tasks that are comepleted or no longer need to be done
+User Story 6: As a user, I can associate tasks with sub-tasks 
+-Create a resource for sub asks with the given information: sub-task name, comepleted or not completed sub-tasks, date, and task_id(foreign key)
+User Story 7: As a user, I can create new sub-tasks into the database
+User Story 8: As a user, I can edit/update existings sub-tasks
+User Story 9: As a user, I can delete sub-tasks if they are comepleted or no longer needed.
+User Story 10: As a user, I can add validations for data in both tables
+
+
+
+
+
+
+
+
+
+
+
+
